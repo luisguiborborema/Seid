ce55ed69d740ae4ffc7139763b8049c69a8843ab
Guilherme_Borborema <76051185+luisguiborborema@users.noreply.github.com>
2025-08-08 23:08:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// assets/js/script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Site da SEID carregado com sucesso.");
+
+    // Futuramente, você pode adicionar aqui:
+    // 1. Lógica para o menu mobile (hambúrguer).
+    // 2. Animações de scroll para revelar elementos.
+    // 3. Validação avançada do formulário de contato.
+    // 4. Um carrossel para os depoimentos ou portfólio.
+});
